978dbd9860d0a3ee0516bb39268784a1b415bed6
genagithub <143031972+genagithub@users.noreply.github.com>
2026-02-16 03:15:06 -0300

    commit

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html, dcc
 from dash.dependencies import Input, Output
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder 
@@ -163,3 +162,4 @@ def update_graph(slct_var_cat,slct_var_num):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050)) 
     app.run_server(host='0.0.0.0', port=port)
+
