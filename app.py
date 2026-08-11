2d0ece4f65541438e72b89c6dde9be6ad72c2312
genagithub <143031972+genagithub@users.noreply.github.com>
2026-08-11 14:16:37 -0300

    commit

--- app.py
+++ app.py
@@ -194,7 +194,7 @@ html.Div(id="dashboard", className="e1_dashboard", children=[
    html.Div(id="insights", className="e1_insights", children=[
        html.Div(f"Total de clientes detectados en riesgo por el modelo: {total_customers_predicted_risk}", id="total_customers", className="e1_txt"),
        html.Div(f"Campañas de pago estratégicamente APROBADAS por ROI: {approved_campaigns}", id="aprove_campaigns", className="e1_txt"),
-       html.Div(f"Campañas RECHAZADAS (Se ahorra pauta o pasa a canal gratuito):", id="reject_campagins", className="e1_txt"),
+       html.Div(f"Campañas RECHAZADAS (Se ahorra pauta o pasa a canal gratuito): {total_customers_predicted_risk - approved_campaigns}", id="reject_campagins", className="e1_txt"),
        html.Div(f"Dinero directo RESCATADO / AHORRADO en presupuesto publicitario: {budget_savings}", id="ROI", className="e1_txt"),
        html.H3("Top 5 clientes a fidelizar", id="H3", className="e1_title"),
        html.Div(id="matrix", className="e1_matrix", children=[
